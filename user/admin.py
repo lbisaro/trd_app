03f292498bf5e1cb167a24d2661eb9f8f284610a
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-03-11 10:13:12 -0300

    Update

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from bot.models import *
+from bot.model_kline import *
+from bot.model_backtest import *
+admin.site.register(Estrategia)
+admin.site.register(Bot)
+admin.site.register(Order)
+admin.site.register(Symbol)
+admin.site.register(Kline)
+admin.site.register(Backtest)
+
