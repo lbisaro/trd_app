4286d0863e0beb772896a1254b20a3fa0d842ad9
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-03-12 09:05:44 -0300

    Update crontab_bot_1m.py

--- scripts/crontab_bot_1m.py
+++ scripts/crontab_bot_1m.py
@@ -10,7 +10,7 @@ from scripts.app_log import app_log as Log
 def run():
     log = Log()
     json_rsp = {}
-    startDt = datetime.now()
+    #startDt = datetime.now()
     #log.info(f'START {startDt}')
     #print(f'START {startDt}')
 
@@ -58,89 +58,101 @@ def run():
     usuario_id = -1
     for bot in bots:
         #print(f'Bot: {bot}')
-        #try:
-        botClass = bot.get_instance()
-        botClass.bot_id = bot.id
-        botClass.username = bot.usuario.username
+        try:
+            botClass = bot.get_instance()
+            botClass.bot_id = bot.id
+            botClass.username = bot.usuario.username
 
-        if bot.usuario.id != usuario_id:
-            #log.info(f'Usuario: {bot.usuario.username}')
+            if bot.usuario.id != usuario_id:
+                #log.info(f'Usuario: {bot.usuario.username}')
 
-            usuario_id = bot.usuario.id
-            profile = UserProfile.objects.get(user_id=bot.usuario.id)
-            profile_config = profile.parse_config()
-            prms = {}
-            prms['bnc_apk'] = profile_config['bnc']['bnc_apk']
-            prms['bnc_aps'] = profile_config['bnc']['bnc_aps']
-            prms['bnc_env'] = profile_config['bnc']['bnc_env']
+                usuario_id = bot.usuario.id
+                profile = UserProfile.objects.get(user_id=bot.usuario.id)
+                profile_config = profile.parse_config()
+                prms = {}
+                prms['bnc_apk'] = profile_config['bnc']['bnc_apk']
+                prms['bnc_aps'] = profile_config['bnc']['bnc_aps']
+                prms['bnc_env'] = profile_config['bnc']['bnc_env']
 
-            exch = Exchange(type='user_apikey',exchange='bnc',prms=prms)                
+                exch = Exchange(type='user_apikey',exchange='bnc',prms=prms)                
 
-        #log.info(f'Bot: {bot}')
+            #log.info(f'Bot: {bot}')
+                
             
-        
-        ### - Disparar las señales a los bots activos
-        ### - Cuando se dispare una señal a un Bot 
-        ###     - Si el bot NO PUEDE EJECUTARLA por cuestiones relacionadas con el capital. Inactivar el Bot
-        signal = 'NEUTRO'
-        signal_row = pd.DataFrame()
-        if bot.estrategia_id in signal_rows:
-            signal_row = signal_rows[bot.estrategia_id]
-            signal = signal_row['signal']
-        if signal != 'NEUTRO':
-            log.info(f'{bot} - aa Signal: {signal}')
+            ### - Disparar las señales a los bots activos
+            ### - Cuando se dispare una señal a un Bot 
+            ###     - Si el bot NO PUEDE EJECUTARLA por cuestiones relacionadas con el capital. Inactivar el Bot
+            signal = 'NEUTRO'
+            signal_row = pd.DataFrame()
+            if bot.estrategia_id in signal_rows:
+                signal_row = signal_rows[bot.estrategia_id]
+                signal = signal_row['signal']
+            if signal != 'NEUTRO':
+                log.info(f'{bot} - aa Signal: {signal}')
 
 
-        #Cargando Billetera del Bot
-        resultados = bot.get_wallet()
-        symbol_info = exch.get_symbol_info(botClass.symbol)
-        qd_qty = symbol_info['qty_decs_qty']
-        qd_quote = symbol_info['qty_decs_quote']
-        botClass.wallet_quote = round(bot.quote_qty + resultados['quote_compras'] + resultados['quote_ventas'] , qd_quote)
-        botClass.wallet_base  = round(resultados['base_compras'] + resultados['base_ventas'] , qd_qty)
-        
-        #Cargando Billetera del Exchange
-        exchange_wallet = exch.get_wallet() 
+            #Cargando Billetera del Bot
+            resultados = bot.get_wallet()
+            symbol_info = exch.get_symbol_info(botClass.symbol)
+            qd_qty = symbol_info['qty_decs_qty']
+            qd_quote = symbol_info['qty_decs_quote']
+            botClass.wallet_quote = round(bot.quote_qty + resultados['quote_compras'] + resultados['quote_ventas'] , qd_quote)
+            botClass.wallet_base  = round(resultados['base_compras'] + resultados['base_ventas'] , qd_qty)
+            
+            #Cargando Billetera del Exchange
+            exchange_wallet = exch.get_wallet() 
 
 
-        #Cargando Ordenes en curso
-        orders = bot.get_orders_en_curso()
-        botClass._trades = {}
-        botClass._orders = {}
-        for order in orders:
-            if order.completed > 0:
-                botClass._trades[order.id] = order
-            else:
-                botClass._orders[order.id] = order
-        
-        # Obtener precios de los symbols activos en cada iteracion de usuario
-        price = exch.get_symbol_price(botClass.symbol)
-        if abs(botClass.wallet_base*price) < 2: #Si el total de qty representa menos de 2 dolares, se toma como 0
-            botClass.wallet_base = 0.0
+            #Cargando Ordenes en curso
+            orders = bot.get_orders_en_curso()
+            botClass._trades = {}
+            botClass._orders = {}
+            for order in orders:
+                if order.completed > 0:
+                    botClass._trades[order.id] = order
+                else:
+                    botClass._orders[order.id] = order
+            
+            # Obtener precios de los symbols activos en cada iteracion de usuario
+            price = exch.get_symbol_price(botClass.symbol)
+            if abs(botClass.wallet_base*price) < 2: #Si el total de qty representa menos de 2 dolares, se toma como 0
+                botClass.wallet_base = 0.0
 
-        #Cargando datos para la ejecucion
-        botClass.signal = signal
-        botClass.row = signal_row
-        botClass.exchange = exch
-        botClass.price = price
-        botClass.exchange_wallet = exchange_wallet
-        execRes = botClass.live_execute()
+            #Cargando datos para la ejecucion
+            botClass.signal = signal
+            botClass.row = signal_row
+            botClass.exchange = exch
+            botClass.price = price
+            botClass.exchange_wallet = exchange_wallet
+            execRes = botClass.live_execute()
 
-        if len(execRes) > 0:
-            log.info(f'Execute: {execRes}')
+            if len(execRes) > 0:
+                log.info(f'Execute: {execRes}')
 
-        bot.make_operaciones()
+            bot.make_operaciones()
 
-        #Procesando estado actual del bot
-        status = botClass.get_status()
-        bot.update_status(status)
+            #Procesando estado actual del bot
+            status = botClass.get_status()
+            bot.update_status(status)
             
 
-        #except Exception as e:
-        #    log.error(f'bot.id: {bot.id} {e}')
-        #    json_rsp['error'].append(f'bot.id: {bot.id} {e}')
+        except Exception as e:
+            log.error(f'bot.id: {bot.id} {e}')
+            json_rsp['error'].append(f'bot.id: {bot.id} {e}')
             
-            
+
+    
+    ### Actualizar velas de los Symbols
+    try:
+        update_klines = exchInfo.update_klines()
+        json_rsp['klines'] = update_klines
+                
+    except Exception as err:
+        err = str(err)
+        msg_text = f'No fue posible encontrar velas\n{err}'
+        json_rsp['error'].append(msg_text)
+
+
     #Buscar ordenes incompletas, agrupadas por usuario
     #Si existen, reconectar con el Exchange para cada usuario 
     # Repetir la busqueda de ordenes incompletas en un bucle para todos los usuarios  
@@ -170,18 +182,8 @@ def run():
 
     #print('Ready')
 
-    endDt = datetime.now()
+    #endDt = datetime.now()
     #log.info(f'END {endDt}')
-    durationDt = endDt-startDt
+    #durationDt = endDt-startDt
     #print('Duracion del proceso: ',durationDt)
 
-    
-    ### Actualizar velas de los Symbols
-    try:
-        update_klines = exchInfo.update_klines()
-        json_rsp['klines'] = update_klines
-                
-    except Exception as err:
-        err = str(err)
-        msg_text = f'No fue posible encontrar velas\n{err}'
-        json_rsp['error'].append(msg_text)
