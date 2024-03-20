dd1d7219308eed030d89215d0115ab65ce086aea
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-03-20 12:23:28 -0300

    Fix

--- bot/views_graph_test.py
+++ bot/views_graph_test.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-#import plotly.express as px
-from scripts.functions import ohlc_chart 
+from scripts.functions import ohlc_chart, plotly_to_json
 import numpy as np
+from django.http import JsonResponse
 
 
 import pickle
-import os
+
 
 # Create your views here.
 def chart(request):
-    klines_file = './www/backtest/klines/2d01/Lateral_BTCUSDT_2d01_2023-06-20_2023-10-09.DataFrame'
+    context = {'chart': chart}
+    return render(request, 'chart.html', context)
+
+def chart_get(request,symbol):
+    klines_file = './backtest/klines/0m15/Lateral_'+symbol+'_0m15_2023-06-20_2023-10-09.DataFrame'
     with open(klines_file, 'rb') as file:
         klines = pickle.load(file)
     
@@ -17,9 +21,6 @@ def chart(request):
     klines["MA_S"] = klines["close"].rolling(100).mean()
     klines['MA_cross'] = np.where((klines['MA_F']>klines['MA_S']) & (klines['MA_F'].shift(1)<klines['MA_S'].shift(1)),klines['MA_F'],None)
     klines['MA_cross'] = np.where((klines['MA_F']<klines['MA_S']) & (klines['MA_F'].shift(1)>klines['MA_S'].shift(1)),klines['MA_F'],klines['MA_cross'])
-
-    
-
     
     indicators = [
         {'col': 'MA_F',
@@ -35,13 +36,16 @@ def chart(request):
         {'col':'MA_cross',
          'name': 'MA Cross',
          'color': 'yellow',
-         'symbol': 'circle-open' #https://plotly.com/python/reference/scatter/#scatter-marker-symbol
+         'symbol': 'circle-open' 
         }
     ]
 
     klines['pnl'] = klines['close']
     fig = ohlc_chart(klines, indicators=indicators,signals=signals)
+    fig_json = plotly_to_json(fig)
 
-    chart = fig.to_html(config = {'scrollZoom': True, }) 
-    context = {'chart': chart}
-    return render(request, 'chart.html', context)
+    json_rsp = {}
+    json_rsp['ok'] = True
+    json_rsp['error'] = False
+    json_rsp['fig_json'] = fig_json
+    return JsonResponse(json_rsp)
