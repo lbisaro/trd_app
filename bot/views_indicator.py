9caa7ad491cfd8c818abb811d5f80866de7be359
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-03-23 18:33:44 -0300

    Update views_indicator.py

--- bot/views_indicator.py
+++ bot/views_indicator.py
@@ -11,6 +11,8 @@ from scripts.functions import get_intervals
 def panel(request):
     symbols = Symbol.objects.filter(activo = 1)
     indicators = Indicator.objects.filter(last=True)
+    for i in indicators:
+        print(i)
     intervals_id = Indicator.intervals
     intervals = []
     for interval_id in intervals_id:
@@ -19,6 +21,13 @@ def panel(request):
         'symbols': symbols,
         'intervals': intervals,
         'indicators': indicators,
+        'INDICATOR_ID_TREND': Indicator.INDICATOR_ID_TREND,
+        'INDICATOR_ID_VOLUME': Indicator.INDICATOR_ID_VOLUME,
+        'INDICATOR_ID_VOLATILITY': Indicator.INDICATOR_ID_VOLATILITY,
+        'indicators_type':[{'id':Indicator.INDICATOR_ID_TREND,'name':'Tendencia'},
+                           {'id':Indicator.INDICATOR_ID_VOLUME,'name':'Volumen'},
+                           {'id':Indicator.INDICATOR_ID_VOLATILITY,'name':'Volatilidad'},
+                          ],
        }
     return render(request, 'indicator_panel.html', context)
 
