53a324f6edee6e922c6cbb666fa16f72c8db45b9
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-03-23 01:31:10 -0300

    Update crontab_main_indicators_1m.py

--- scripts/crontab_main_indicators_1m.py
+++ scripts/crontab_main_indicators_1m.py
@@ -8,14 +8,8 @@ from datetime import datetime
 
 
 def run():
-    startDt = datetime.now()
-    hr = startDt.strftime('%H')
-    mn = startDt.strftime('%M')
-    
-    if mn[1]=='0': #Solo entra en los minutos multiplo de 10 / Representa que se ejecuta cada 0 minutos
 
-        print('Revisndo indicadores ',startDt)
-        indicator = Indicator()
-        indicator.update()
+    indicator = Indicator()
+    indicator.update()
 
     
