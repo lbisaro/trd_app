c4762829c289482e0b82ff9a8dceeb0b5adfa00f
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-11-12 21:30:26 -0300

    Update BotHeikinAshi2.py

--- scripts/bots/BotHeikinAshi2.py
+++ scripts/bots/BotHeikinAshi2.py
@@ -67,10 +67,12 @@ class BotHeikinAshi2(Bot_Core):
         df = HeikinAshi(self.klines)
 
         #V2 mejorada
-        df['HA_sl'] =  np.where((df['HA_side']>=0) & (df['HA_side'].shift(1)>=0),df['HA_low'].shift(1),None)
-        df['HA_tp'] =  np.where((df['HA_side']<0) & (df['HA_side'].shift(1)<=0),df['HA_high'].shift(2),None)
-        df['HA_sl'].ffill(inplace=True)
-        df['HA_tp'].ffill(inplace=True)
+        #df['HA_sl'] =  np.where((df['HA_side']>=0) & (df['HA_side'].shift(1)>=0),df['HA_low'].shift(1),None)
+        #df['HA_tp'] =  np.where((df['HA_side']<0) & (df['HA_side'].shift(1)<=0),df['HA_high'].shift(2),None)
+        #df['HA_sl'].ffill(inplace=True)
+        #df['HA_tp'].ffill(inplace=True)
+        df['HA_sl'] =  df['HA_low'].shift(2)
+        df['HA_tp'] =  df['HA_high'].shift(2)
         buy_cond = ( 
            (df['HA_sl']>df['HA_sl'].shift(1))  
            & (df['HA_sl'].shift(2)>df['HA_sl'].shift(1)) 
@@ -79,7 +81,7 @@ class BotHeikinAshi2(Bot_Core):
            & (df['HA_side'].shift(5)<=0) 
            & (df['HA_side'].shift(6)<=0) 
            )
-        #buy_cond = (df['HA_sl']>df['HA_sl'].shift(1)) & (df['HA_sl'].shift(2)>df['HA_sl'].shift(1))
+        #`buy_cond = (df['HA_sl']>df['HA_sl'].shift(1)) & (df['HA_sl'].shift(2)>df['HA_sl'].shift(1))
         #buy_cond = (df['HA_sl']>df['HA_tp']) & (df['HA_sl'].shift(1)<df['HA_tp'].shift(1)) & (df['HA_sl']!=df['HA_sl'].shift(1))
         df['buy'] = np.where(buy_cond,1,None)
 
