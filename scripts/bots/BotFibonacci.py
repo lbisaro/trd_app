eb2c26426e8dca56987bcf232f771184e89c05e5
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-11-25 16:11:29 -0300

    Update Fibonacci

--- scripts/bots/BotFibonacci.py
+++ scripts/bots/BotFibonacci.py
@@ -1,7 +1,7 @@
 from bot.model_kline import *
 import numpy as np
 from scripts.Bot_Core import Bot_Core
-from scripts.indicators import fibonacci_levels,zigzag
+from scripts.indicators import zigzag, fibonacci_extension
 from scripts.functions import round_down
 from scripts.Bot_Core_utils import *
 
@@ -11,11 +11,18 @@ class BotFibonacci(Bot_Core):
     symbol = ''
     quote_perc = 0.0
     interes = ''
-
-    #Gestion de ordenes y posicion tomada
-    position = False #Define si existe una posicion abierta
-    orderid_sl = 0 #id de la orden de Stop-Loss
-    orderid_tp = 0 #id de la orden de Take-Profit
+    fb_levels = [0.0,        
+                 0.236,      
+                 0.382, 
+                 0.5, 
+                 0.618, 
+                 0.786,
+                 1.0, 
+                 1.272, 
+                 1.618, 
+                 2.0, 
+                 2.618
+                 ]
 
     def __init__(self):
         self.symbol = ''
@@ -24,15 +31,10 @@ class BotFibonacci(Bot_Core):
         self.take_profit = 0.0
         self.interes = 's'  
 
-        #Gestion de ordenes y posicion tomada
-        self.position = False #Define si existe una posicion abierta
-        self.orderid_sl = 0 #id de la orden de Stop-Loss
-        self.orderid_tp = 0 #id de la orden de Take-Profit
 
     descripcion = 'Bot Core v2 \n'\
-                  'Ejecuta la compra al recibir una señal de Compra y Venta por retroceso de Fibonacci, con stoploss en nivel 0.0 , '\
-                  'Cierra la operación por Stop Loss o Señal de venta.'\
-                  'El parametro -Desvio para Pivotes- se utiliza para definir los pivotes de precio con los que se calculan los retrocesos. '
+                  'Ejecuta la compra al recibir una señal de Compra por Extension de Fibonacci, con stoploss en nivel 0.0 , '\
+                  'Cierra la operación por Stop Loss o Señal de venta.'
     
     parametros = {'symbol':  {  
                         'c' :'symbol',
@@ -51,7 +53,7 @@ class BotFibonacci(Bot_Core):
                 'interes': {
                         'c' :'interes',
                         'd' :'Tipo de interes',
-                        'v' :'c',
+                        'v' :'s',
                         't' :'t_int',
                         'pub': True,
                         'sn':'Int', },
@@ -69,63 +71,45 @@ class BotFibonacci(Bot_Core):
         
     def start(self):
         
-        self.klines = zigzag(self.klines)
+        self.klines = zigzag(self.klines, resample_periods=8)
+ 
+        #Detectando pivots para analisis de fibonacci
+        pivots = self.klines[self.klines['ZigZag']>0].copy()
+        pivots['pivots'] = pivots['ZigZag'].copy()
+        pivots['fb_0'] = pivots['ZigZag'].copy()
+        pivots['fb_1'] = pivots['ZigZag'].shift(1)
+        pivots['fb_2'] = pivots['ZigZag'].shift(2)
 
-        self.klines['signal'] = ''
-        self.fibo_sl = '0.0%'
-        self.fibo_gold_h = '38.2%'
-        self.fibo_gold_l = '23.6%'
-        self.fibo_tp = '100.0%'
-        self.klines[self.fibo_sl] = None
-        self.klines[self.fibo_gold_h] = None
-        self.klines[self.fibo_gold_l] = None
-        self.klines[self.fibo_tp] = None
-        
+        #Cambios de tendencia
+        pivots['trend'] = np.where((pivots['fb_0']>pivots['fb_2']) & (pivots['fb_1']>pivots['fb_0']), 2,0)
+        pivots['trend'] = np.where((pivots['fb_0']>pivots['fb_2']) & (pivots['fb_1']<pivots['fb_0']), 1,pivots['trend'])
+        pivots['trend'] = np.where((pivots['fb_0']<pivots['fb_2']) & (pivots['fb_1']<pivots['fb_0']),-2,pivots['trend'])
+        pivots['trend'] = np.where((pivots['fb_0']<pivots['fb_2']) & (pivots['fb_1']>pivots['fb_0']),-1,pivots['trend'])
+        first_pivot_index = pivots.index[0]
+        pivots.at[first_pivot_index,'trend'] = 0 #El primer pivot no se puede definir por lo tanto queda neutro
 
-        last_zz = [0,0,0]
-        buy_last_low = None
-        buy_last_high = None
-        sell_last_low = None
-        sell_last_high = None
+        #Completando el datafame principal con los datos de pivots
+        df_cols = self.klines.columns.copy()
+        for col in pivots.columns:
+            if not col in df_cols:
+                self.klines[col]=None
+        for index, row in pivots.iterrows():
+            for col in pivots.columns:
+                if not col in df_cols:
+                    self.klines.at[index,col] = row[col]
 
-        for index, row in self.klines.iterrows():
-            if row['ZigZag'] is not None and row['ZigZag'] > 0:
-                                
-                last_zz[2] = last_zz[1]
-                last_zz[1] = last_zz[0]
-                last_zz[0] = row['ZigZag']
+        self.klines['signal'] = np.where(self.klines['trend']==2, 'COMPRA' , 'NEUTRO')
+        self.klines['signal'] = np.where(self.klines['trend']==-2, 'VENTA' , self.klines['signal'])
 
-                if last_zz[1] > last_zz[0] > last_zz[2]:
-                    buy_last_low = last_zz[0]
-                    buy_last_high = last_zz[1]
-                
-                if last_zz[1] < last_zz[0] < last_zz[2]:
-                    sell_last_low = last_zz[1]
-                    sell_last_high = last_zz[0]
-                        
-            if buy_last_low is not None and buy_last_high is not None:
-                fib_levels = fibonacci_levels(buy_last_high,buy_last_low)
-                self.klines.at[index, self.fibo_sl] = fib_levels[self.fibo_sl]
-                self.klines.at[index, self.fibo_tp] = fib_levels[self.fibo_tp]
-                self.klines.at[index, self.fibo_gold_h] = fib_levels[self.fibo_gold_h]
-                self.klines.at[index, self.fibo_gold_l] = fib_levels[self.fibo_gold_l]
-                
-                if fib_levels[self.fibo_gold_l] <= row['close'] <= fib_levels[self.fibo_gold_h]:
-                    self.klines.at[index, 'signal'] = 'COMPRA'
-                    buy_last_low = None
-                    buy_last_high = None
+        self.klines['trend'].ffill(inplace=True)
+        self.klines['fb_0'].ffill(inplace=True)
+        self.klines['fb_1'].ffill(inplace=True)
+        self.klines['fb_2'].ffill(inplace=True)
 
-            if sell_last_low is not None and sell_last_high is not None:
-                fib_levels = fibonacci_levels(sell_last_low,sell_last_high)
-                self.klines.at[index, self.fibo_sl] = fib_levels[self.fibo_sl]
-                self.klines.at[index, self.fibo_tp] = fib_levels[self.fibo_tp]
-                self.klines.at[index, self.fibo_gold_h] = fib_levels[self.fibo_gold_h]
-                self.klines.at[index, self.fibo_gold_l] = fib_levels[self.fibo_gold_l]
+        self.klines['long_fbe_0'] = np.where(self.klines['trend']==2, self.klines['fb_0'] , None)
+        self.klines['long_fbe_1'] = np.where(self.klines['trend']==2, self.klines['fb_1'] , None)
+        self.klines['long_fbe_2'] = np.where(self.klines['trend']==2, self.klines['fb_2'] , None)
 
-                if fib_levels[self.fibo_gold_l] >= row['close'] >= fib_levels[self.fibo_gold_h]:
-                    self.klines.at[index, 'signal'] = 'VENTA'
-                    sell_last_low = None
-                    sell_last_high = None
 
         self.print_orders = False
         self.graph_open_orders = True
@@ -135,48 +119,80 @@ class BotFibonacci(Bot_Core):
         signal = self.signal
         price = self.price
 
-        #Si no existe la orden de SL o TP es porque se ejecuto, entonces se cancelan las ordenes abiertas y se resetea la posicion
-        if (self.orderid_sl > 0 and self.orderid_sl not in self._orders) or (self.orderid_tp > 0 and self.orderid_tp not in self._orders) :
-            self.cancel_orders()
-            self.position = False
+        self.position = False
+        if 'pos___base_qty' in self.status:
+            if self.status['pos___base_qty']['r'] > 0:
+                self.position = True
             
         if not self.position:
             if signal == 'COMPRA':
-                #Analisis del riesgo a tomar
-                stop_loss_price = round(self.row[self.fibo_sl] , self.qd_price)
-                self.stop_loss = round((1-(stop_loss_price/self.price))*100,2)
 
-                take_profit_price = round(self.row[self.fibo_tp] , self.qd_price)
-                self.take_profit = round(((take_profit_price/self.price)-1)*100,2)
-                
-                if self.interes == 's': #Interes Simple
+                take_profit_price = 0
+                stop_loss_price = 0
+                pre_level = -1.0
+                max_i = len(self.fb_levels)-1
+                for i, level in enumerate(self.fb_levels):
+                    if level > 0:
+                        level_price = fibonacci_extension(self.row['long_fbe_0'],self.row['long_fbe_1'],self.row['long_fbe_2'],level)
+                        pre_level_price = fibonacci_extension(self.row['long_fbe_0'],self.row['long_fbe_1'],self.row['long_fbe_2'],pre_level)
+                        if level_price > self.price > pre_level_price:
+                            if i<max_i:
+                                take_profit_price = fibonacci_extension(self.row['long_fbe_0'],self.row['long_fbe_1'],self.row['long_fbe_2'],self.fb_levels[i+1])
+                            else:
+                                take_profit_price = level_price
+                            #stop_loss_price = pre_level_price
+                            stop_loss_price = fibonacci_extension(self.row['long_fbe_0'],self.row['long_fbe_1'],self.row['long_fbe_2'],level=0.0)
+                    pre_level = level
+
+                if take_profit_price>0 and stop_loss_price>0:
+
+                    #PENDIENTE - Analisis del riesgo a tomar
+                    stop_loss_price = round_down(stop_loss_price , self.qd_price)
+                    self.stop_loss = round((1-(stop_loss_price/self.price))*100,2)
+
+                    take_profit_price = round(take_profit_price , self.qd_price)
+                    self.take_profit = round(((take_profit_price/self.price)-1)*100,2)
                     
-                    quote_qty = self.quote_qty if self.wallet_quote >= self.quote_qty else self.wallet_quote
-                    quote_to_sell = round_down(quote_qty*(self.quote_perc/100) , self.qd_quote )
-                elif self.interes == 'c': #Interes Compuesto
-                    quote_to_sell = round_down(self.wallet_quote*(self.quote_perc/100) , self.qd_quote ) 
-                
-                quote_to_sell = round_down(quote_to_sell , self.qd_quote ) 
-                base_to_buy = round_down((quote_to_sell/price) , self.qd_qty) 
-            
-                orderid_buy = self.buy(base_to_buy,Order.FLAG_SIGNAL)
-                if orderid_buy > 0:
-
-                    buyed_qty = self._trades[orderid_buy].qty
+                    if self.interes == 's': #Interes Simple
+                        quote_qty = self.quote_qty if self.wallet_quote >= self.quote_qty else self.wallet_quote
+                        quote_to_sell = round_down(quote_qty*(self.quote_perc/100) , self.qd_quote )
+                    elif self.interes == 'c': #Interes Compuesto
+                        quote_to_sell = round_down(self.wallet_quote*(self.quote_perc/100) , self.qd_quote ) 
                     
-                    self.position = True
-                    self.orderid_sl = self.sell_limit(buyed_qty,Order.FLAG_STOPLOSS,stop_loss_price)
-                    #self.orderid_tp = self.sell_limit(buyed_qty,Order.FLAG_TAKEPROFIT,take_profit_price) 
+                    quote_to_sell = round_down(quote_to_sell , self.qd_quote ) 
+                    base_to_buy = round_down((quote_to_sell/price) , self.qd_qty) 
                 
-                    if self.orderid_sl == 0:
-                        print('\033[31mERROR\033[0m',self.row['datetime'],'STOP-LOSS',buyed_qty,' ',quote_to_sell,self.wallet_quote)  
-                    #if self.orderid_tp == 0:
-                    #    print('\033[31mERROR\033[0m',self.row['datetime'],'TAKE-PROFIT',buyed_qty,' ',quote_to_sell,self.wallet_quote) 
-                
-                else:
-                    print('\033[31mERROR\033[0m',self.row['datetime'],'BUY price',self.price,'USD',quote_to_sell,self.wallet_quote)
+                    orderid_buy = self.buy(base_to_buy,Order.FLAG_SIGNAL)
+                    if orderid_buy > 0:
 
-        if signal == 'VENTA':
+                        buyed_qty = self._trades[orderid_buy].qty
+                        
+                        self.position = True
+                        self.orderid_sl = self.sell_limit(buyed_qty,Order.FLAG_STOPLOSS,stop_loss_price)
+                        #self.orderid_tp = self.sell_limit(buyed_qty,Order.FLAG_TAKEPROFIT,take_profit_price) 
+                    
+                        if self.orderid_sl == 0:
+                            print('\033[31mERROR\033[0m',self.row['datetime'],'STOP-LOSS',buyed_qty,' ',quote_to_sell,self.wallet_quote)  
+                        #if self.orderid_tp == 0:
+                        #    print('\033[31mERROR\033[0m',self.row['datetime'],'TAKE-PROFIT',buyed_qty,' ',quote_to_sell,self.wallet_quote) 
+                    
+                    else:
+                        print('\033[31mERROR\033[0m',self.row['datetime'],'BUY price',self.price,'USD',quote_to_sell,self.wallet_quote)
+
+        if self.position and signal == 'VENTA':
             self.close(Order.FLAG_SIGNAL)
             self.cancel_orders()
             self.position = False
+
+        mddpos = 4
+        if 'pos___pnl_max' in self.status:
+            if self.status['pos___pnl_max']['r'] > mddpos*4:
+                mddpos = self.status['pos___pnl_max']['r']/4
+        if 'pos___pnl' in self.status and self.status['pos___pnl_max']['r']> mddpos:
+            if self.status['pos___pnl_max']['r']-self.status['pos___pnl']['r'] > mddpos:
+                self.close(flag=Order.FLAG_TAKEPROFIT)
+                self.cancel_orders()
+
+    def on_order_execute(self, order):
+        if order.side == Order.SIDE_SELL:
+            self.cancel_orders()
---
+++ scripts/bots/BotFibonacci_old.py
@@ -0,0 +1,191 @@
+from bot.model_kline import *
+import numpy as np
+from scripts.Bot_Core import Bot_Core
+from scripts.indicators import fibonacci_levels,zigzag
+from scripts.functions import round_down
+from scripts.Bot_Core_utils import *
+
+class BotFibonacci_old(Bot_Core):
+
+    short_name = 'FiboOld'
+    symbol = ''
+    quote_perc = 0.0
+    interes = ''
+
+    #Gestion de ordenes y posicion tomada
+    position = False #Define si existe una posicion abierta
+    orderid_sl = 0 #id de la orden de Stop-Loss
+    orderid_tp = 0 #id de la orden de Take-Profit
+
+    def __init__(self):
+        self.symbol = ''
+        self.quote_perc = 0.0
+        self.stop_loss = 0.0
+        self.take_profit = 0.0
+        self.interes = 's'  
+
+        #Gestion de ordenes y posicion tomada
+        self.position = False #Define si existe una posicion abierta
+        self.orderid_sl = 0 #id de la orden de Stop-Loss
+        self.orderid_tp = 0 #id de la orden de Take-Profit
+
+    descripcion = 'Bot Core v2 \n'\
+                  'Ejecuta la compra al recibir una señal de Compra y Venta por retroceso de Fibonacci, con stoploss en nivel 0.0 , '\
+                  'Cierra la operación por Stop Loss o Señal de venta.'\
+                  'El parametro -Desvio para Pivotes- se utiliza para definir los pivotes de precio con los que se calculan los retrocesos. '
+    
+    parametros = {'symbol':  {  
+                        'c' :'symbol',
+                        'd' :'Par',
+                        'v' :'BTCUSDT',
+                        't' :'symbol',
+                        'pub': True,
+                        'sn':'Sym',},
+                 'quote_perc': {
+                        'c' :'quote_perc',
+                        'd' :'Operacion sobre capital',
+                        'v' :'95',
+                        't' :'perc',
+                        'pub': True,
+                        'sn':'Quote', },
+                'interes': {
+                        'c' :'interes',
+                        'd' :'Tipo de interes',
+                        'v' :'s',
+                        't' :'t_int',
+                        'pub': True,
+                        'sn':'Int', },
+                }
+
+    def valid(self):
+        err = []
+        if len(self.symbol) < 1:
+            err.append("Se debe especificar el Par")
+        if self.quote_perc <= 0 or self.quote_perc > 100:
+            err.append("El Porcentaje de capital por operacion debe ser un valor entre 0.01 y 100")
+
+        if len(err):
+            raise Exception("\n".join(err))
+        
+    def start(self):
+        
+        self.klines = zigzag(self.klines)
+
+        self.klines['signal'] = ''
+        self.fibo_sl = '0.0%'
+        self.fibo_gold_h = '38.2%'
+        self.fibo_gold_l = '23.6%'
+        self.fibo_tp = '100.0%'
+        self.klines[self.fibo_sl] = None
+        self.klines[self.fibo_gold_h] = None
+        self.klines[self.fibo_gold_l] = None
+        self.klines[self.fibo_tp] = None
+        
+
+        last_zz = [0,0,0]
+        buy_last_low = None
+        buy_last_high = None
+        sell_last_low = None
+        sell_last_high = None
+
+        for index, row in self.klines.iterrows():
+            if row['ZigZag'] is not None and row['ZigZag'] > 0:
+                                
+                last_zz[2] = last_zz[1]
+                last_zz[1] = last_zz[0]
+                last_zz[0] = row['ZigZag']
+
+                if last_zz[1] > last_zz[0] > last_zz[2]:
+                    buy_last_low = last_zz[0]
+                    buy_last_high = last_zz[1]
+                
+                if last_zz[1] < last_zz[0] < last_zz[2]:
+                    sell_last_low = last_zz[1]
+                    sell_last_high = last_zz[0]
+                        
+            if buy_last_low is not None and buy_last_high is not None:
+                fib_levels = fibonacci_levels(buy_last_high,buy_last_low)
+                self.klines.at[index, self.fibo_sl] = fib_levels[self.fibo_sl]
+                self.klines.at[index, self.fibo_tp] = fib_levels[self.fibo_tp]
+                self.klines.at[index, self.fibo_gold_h] = fib_levels[self.fibo_gold_h]
+                self.klines.at[index, self.fibo_gold_l] = fib_levels[self.fibo_gold_l]
+                
+                if fib_levels[self.fibo_gold_l] <= row['close'] <= fib_levels[self.fibo_gold_h]:
+                    self.klines.at[index, 'signal'] = 'COMPRA'
+                    buy_last_low = None
+                    buy_last_high = None
+
+            if sell_last_low is not None and sell_last_high is not None:
+                fib_levels = fibonacci_levels(sell_last_low,sell_last_high)
+                self.klines.at[index, self.fibo_sl] = fib_levels[self.fibo_sl]
+                self.klines.at[index, self.fibo_tp] = fib_levels[self.fibo_tp]
+                self.klines.at[index, self.fibo_gold_h] = fib_levels[self.fibo_gold_h]
+                self.klines.at[index, self.fibo_gold_l] = fib_levels[self.fibo_gold_l]
+
+                if fib_levels[self.fibo_gold_l] >= row['close'] >= fib_levels[self.fibo_gold_h]:
+                    self.klines.at[index, 'signal'] = 'VENTA'
+                    sell_last_low = None
+                    sell_last_high = None
+
+        self.print_orders = False
+        self.graph_open_orders = True
+
+
+    def next(self):
+        signal = self.signal
+        price = self.price
+
+        #Si no existe la orden de SL o TP es porque se ejecuto, entonces se cancelan las ordenes abiertas y se resetea la posicion
+        if (self.orderid_sl > 0 and self.orderid_sl not in self._orders) or (self.orderid_tp > 0 and self.orderid_tp not in self._orders) :
+            self.cancel_orders()
+            self.position = False
+            
+        if not self.position:
+            if signal == 'COMPRA':
+                #Analisis del riesgo a tomar
+                stop_loss_price = round(self.row[self.fibo_sl] , self.qd_price)
+                self.stop_loss = round((1-(stop_loss_price/self.price))*100,2)
+
+                take_profit_price = round(self.row[self.fibo_tp] , self.qd_price)
+                self.take_profit = round(((take_profit_price/self.price)-1)*100,2)
+                
+                if self.interes == 's': #Interes Simple
+                    
+                    quote_qty = self.quote_qty if self.wallet_quote >= self.quote_qty else self.wallet_quote
+                    quote_to_sell = round_down(quote_qty*(self.quote_perc/100) , self.qd_quote )
+                elif self.interes == 'c': #Interes Compuesto
+                    quote_to_sell = round_down(self.wallet_quote*(self.quote_perc/100) , self.qd_quote ) 
+                
+                quote_to_sell = round_down(quote_to_sell , self.qd_quote ) 
+                base_to_buy = round_down((quote_to_sell/price) , self.qd_qty) 
+            
+                orderid_buy = self.buy(base_to_buy,Order.FLAG_SIGNAL)
+                if orderid_buy > 0:
+
+                    buyed_qty = self._trades[orderid_buy].qty
+                    
+                    self.position = True
+                    self.orderid_sl = self.sell_limit(buyed_qty,Order.FLAG_STOPLOSS,stop_loss_price)
+                    #self.orderid_tp = self.sell_limit(buyed_qty,Order.FLAG_TAKEPROFIT,take_profit_price) 
+                
+                    if self.orderid_sl == 0:
+                        print('\033[31mERROR\033[0m',self.row['datetime'],'STOP-LOSS',buyed_qty,' ',quote_to_sell,self.wallet_quote)  
+                    #if self.orderid_tp == 0:
+                    #    print('\033[31mERROR\033[0m',self.row['datetime'],'TAKE-PROFIT',buyed_qty,' ',quote_to_sell,self.wallet_quote) 
+                
+                else:
+                    print('\033[31mERROR\033[0m',self.row['datetime'],'BUY price',self.price,'USD',quote_to_sell,self.wallet_quote)
+
+        if signal == 'VENTA':
+            self.close(Order.FLAG_SIGNAL)
+            self.cancel_orders()
+            self.position = False
+
+        mddpos = 4
+        if 'pos___pnl_max' in self.status:
+            if self.status['pos___pnl_max']['r'] > mddpos*4:
+                mddpos = self.status['pos___pnl_max']['r']/4
+        if 'pos___pnl' in self.status and self.status['pos___pnl_max']['r']> mddpos:
+            if self.status['pos___pnl_max']['r']-self.status['pos___pnl']['r'] > mddpos:
+                self.close(flag=Order.FLAG_TAKEPROFIT)
+                self.cancel_orders()
