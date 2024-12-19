c27829c5a25584fb3af772940138caa560e4caa0
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-12-19 20:18:07 -0300

    Update crontab.bat

--- crontab.bat
+++ crontab.bat
@@ -2,3 +2,4 @@
 cd "C:\Users\lbisa\Mi unidad (leonardo.bisaro@gmail.com)\Cripto\python\trd_app"
 python .\manage.py runscript crontab_bot_1m
 python .\manage.py runscript crontab_main_indicators_1m
+python .\manage.py runscript crontab_check_price_change
