10e217c65bd2c14d4c894db86ba7bf0e3a4e6d49
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2024-12-19 21:36:25 -0300

    Update crontab_check_price_change.py

--- scripts/crontab_check_price_change.py
+++ scripts/crontab_check_price_change.py
@@ -40,13 +40,14 @@ def run():
     # Obtener prices actuales
     actual_prices = {}
     tickers = exch.client.get_ticker()
-    for ticker in tickers:
-        symbol = ticker['symbol']
-        close_time = datetime.fromtimestamp(ticker['closeTime']/1000).date()
-        check_proc_date = datetime.strptime(proc_date, '%Y-%m-%d').date()
-        diff_days = abs((check_proc_date - close_time).days)
-        if symbol.endswith(USDT_PAIR) and diff_days==0:
-            actual_prices[symbol] = float(ticker['lastPrice'])
+    if len(tickers) > 0:
+        for ticker in tickers:
+            symbol = ticker['symbol']
+            close_time = datetime.fromtimestamp(ticker['closeTime']/1000).date()
+            check_proc_date = datetime.strptime(proc_date, '%Y-%m-%d').date()
+            diff_days = abs((check_proc_date - close_time).days)
+            if symbol.endswith(USDT_PAIR) and diff_days==0:
+                actual_prices[symbol] = float(ticker['lastPrice'])
 
     # Cargar data previos
     data = load_data_file(DATA_FILE)
