70f7ba4b31c62bac97a25de0c10452e729676bd2
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2025-05-14 22:12:33 -0300

    Update indicators.py

--- scripts/indicators.py
+++ scripts/indicators.py
@@ -568,3 +568,40 @@ def HeikinAshi(df):
     df.drop(['HA_vela_indecision','HA_cuerpo','HA_sombra_inferior','HA_sombra_superior'], axis=1, inplace=True)
     return df
 
+def get_pivots_alert(df,threshold=1):
+    
+    data = {}
+    data['alert'] = 0
+    periods = 200
+    if df['close'].count() >= periods:
+        
+        df = zigzag(df)
+        
+        pivots = df[df['ZigZag']>0]['ZigZag'].tolist()
+
+        #pivots.append(last_close)
+        if len(pivots) > 0:
+            if len(pivots) >= 5:
+
+                #Busqueda de pivots con el siguiente formato (1% o mas entre ) 
+                #                -2
+                #                    -1
+                #        -4
+                #            -3
+                #    -5              
+
+                #Minimos en aumento
+                if pivots[-2]>pivots[-1]*(1+threshold/100) and pivots[-1]>pivots[-4]*(1+threshold/100) and\
+                   pivots[-4]>pivots[-3]*(1+threshold/100) and pivots[-3]>pivots[-5]*(1+threshold/100) :
+                    amplitud = (pivots[-2]/pivots[-3]-1)*100
+                    retroceso = (pivots[-2]/pivots[-1]-1)*100
+                    data['alert'] = 1
+                    data['side'] = 1
+                    data['alert_str'] = 'Minimos en aumento'
+                    data['perc_amplitud'] = amplitud
+                    data['perc_retroceso'] = retroceso
+                    data['sl1'] = pivots[-1]
+                    data['tp1'] = pivots[-2] 
+                    data['in_price'] = data['sl1']+((data['tp1']-data['sl1'])/3) #Genera un ratio 2:1
+        
+    return data
