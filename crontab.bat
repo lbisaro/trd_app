cd8280ca4696bcf50b2d964bec68d1ec2b944e97
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2025-07-14 12:24:51 -0300

    Update crontab.bat

--- crontab.bat
+++ crontab.bat
@@ -1,7 +1,7 @@
 @echo off
 cd "C:\Users\lbisa\Mi unidad (leonardo.bisaro@gmail.com)\Cripto\python\trd_app"
-REM python .\manage.py runscript crontab_bot_1m
+python .\manage.py runscript crontab_bot_1m
 REM python .\manage.py runscript crontab_main_indicators_1m
 REM python .\manage.py runscript crontab_check_price_change
 REM python .\manage.py runscript crontab_order_book
-python .\manage.py runscript crontab_futures_alerts
+REM python .\manage.py runscript crontab_futures_alerts
